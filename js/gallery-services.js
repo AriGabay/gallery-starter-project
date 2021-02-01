e18b00580eccf83539a1b84bd231ffd29448d31c
AriGabay <arigabay19988@gmail.com>
2021-02-01 18:26:33 +0200

    update modals and grid

---
+++ js/gallery-services.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var gProjects = [
+  {
+    id: 'book-shop',
+    name: 'Book-Shop',
+    title: 'Shoe Store Inventory Management Panel',
+    desc: 'Shoe Store Inventory Management Panel',
+    url: 'https://arigabay.github.io/book-shop/',
+    img: 'img/img_project/book-shop.jpg',
+    publishedAt: 1448693940000,
+    labels: ['Pannel', 'Tables', 'Data Control'],
+  },
+  {
+    id: 'guessMe',
+    name: 'GuessMe',
+    title: 'GuessMe Game',
+    desc:
+      'Who Am I? is a guessing game where players use yes or no questions to guess the identity of a famous person. Questions are based upon the traits and characteristics of a person everyone will be able to identify. This game works well with any size group, however the larger the group, the more fun the game becomes.',
+    url: 'https://arigabay.github.io/GuessMe/',
+    img: 'img/img_project/guessMe.jpg',
+    publishedAt: 1448693940000,
+    labels:  ['Game', 'Mouse events'],
+  },
+  {
+    id: 'pacman',
+    name: 'Pacman',
+    title: 'Pacman Game',
+    desc: 'Pacman is a maze chase video game; the player controls the eponymous character through an enclosed maze. The objective of the game is to eat all of the dots placed in the maze while avoiding four colored ghosts',
+    url: 'https://arigabay.github.io/pacman/',
+    img: 'img/img_project/pacman.jpg',
+    publishedAt: 1448693940000,
+    labels: ['Game', 'Matrixes', 'keyboard events'],
+  },
+  {
+    id: 'minesweeper',
+    name: 'Minesweeper',
+    title: 'Minesweeper Game',
+    desc: 'In Minesweeper, mines (that resemble naval mines in the classic theme) are scattered throughout a board, which is divided into cells. Cells have three states: uncovered, covered and flagged. A covered cell is blank and clickable, while an uncovered cell is exposed. Flagged cells are those marked by the player to indicate a potential mine location. A player left-clicks a cell to uncover it. If a player uncovers a mined cell, the game ends, as there is only 1 life per game. Otherwise, the uncovered cells displays either a number, indicating the quantity of mines diagonally or orthogonally adjacent to it, or a blank tile (or Zero), and all adjacent non-mined cells will automatically be uncovered. Right-clicking on a cell will flag it, causing a flag to appear on it. Flagged cells are still considered covered, and a player can click on them to uncover them, although typically they must first be unflagged with an additional right-click. The first click in any game will never be a mine. To win the game, players must uncover all non-mine cells, at which point, the timer is stopped. Flagging all the mined cells is not required.Shoe Store Inventory Management Panel',
+    url: 'https://arigabay.github.io/minesweeper-/',
+    img: 'img/img_project/minesweeper.jpg',
+    publishedAt: 1448693940000,
+    labels: ['Game', 'Matrixes', 'Mouse Click events'],
+  },
+];
+
+
